e68fccbef2fcceba731ce620bb0410a44735f458
Victor Machado da Silva <31703941+victor0machado@users.noreply.github.com>
2020-04-20 21:22:21 -0300

    Incluído ponto de entrada da aplicação

---
+++ sim.py
@@ -0,0 +1,11 @@
+"""Application entry point."""
+from simulator import main as simulator
+
+
+def main() -> None:
+    """Entry point."""
+    simulator()
+
+
+if __name__ == "__main__":
+    main()

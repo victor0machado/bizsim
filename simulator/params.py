f354a85dd099a4e8072d754d48f44a8ed3391d55
Victor Machado da Silva <31703941+victor0machado@users.noreply.github.com>
2020-05-19 16:26:28 -0300

    Ajustando o gerenciador de parâmetros para ler um arquivo inicial e manter um outro em cache

--- simulator/params.py
+++ simulator/params.py
@@ -8,21 +8,29 @@ PARAM_PATH = path.join(SRC_PATH, 'params.json')
 
 
 class ParamManager():
-    def __init__(self) -> None:
-        self.manager = _cache_manager()
+    def __init__(self, input_path: str) -> None:
+        self.manager = _get_jsonfile(PARAM_PATH)
+        self.get_input_data(input_path)
 
     def get_value(self, param: str):
         return self.manager.get(param, None)
 
+    # Descobrir como atribui um tipo generico pra value
     def set_value(self, param: str, value) -> None:
         self.manager[param] = value
         with open(PARAM_PATH, 'w', encoding='utf-8') as fd:
             json.dump(self.manager, fd, indent=4)
 
+    def get_input_data(self, input_path: str) -> None:
+        params = _get_jsonfile(input_path)
+        for param, value in params.items():
+            self.set_value(param, value)
 
-def _cache_manager() -> dict:
-    """Store .json file saved on PARAM_PATH."""
+
+def _get_jsonfile(filepath: str) -> dict:
+    """Store .json file saved on a given path."""
     ret = {}
-    with open(PARAM_PATH, 'r', encoding='utf-8') as fd:
-        ret = json.load(fd)
+    if path.isfile(filepath):
+        with open(filepath, 'r', encoding='utf-8') as fd:
+            ret = json.load(fd)
     return ret
